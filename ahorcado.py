e2aeda1c995d336dd39fa8cc48efc5f09f6185d1
Jaime Burbano <burbanojaime98@gmail.com>
2020-03-04 17:09:24 -0500

    imagenes ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,10 +1,82 @@
 # coding=utf-8
+import random
 
-IMAGENES = []
+IMAGES = ['''
+
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        =========''', '''
+''']
+
 WORDS = ['vaca', "celular", "amor", "principe", "duque", "desarrollo", "diseno"]
 
+def random_word():
+    number = random.randint(0, len(WORDS))
+    print(number)
+    return word
+
 def run():
-    print("gola")
+    word = random_word()
 
 if __name__ == '__main__':
     print("- B I E N V E N I D O S  A  A H O R C A D O S -")

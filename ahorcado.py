e79e4507091df1f2be9fccec558e220e73a3f08a
Jaime Burbano <burbanojaime98@gmail.com>
2020-03-04 13:20:10 -0500

    empezar ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+IMAGENES = []
+WORDS = ['vaca', "celular", "amor", "principe", "duque", "desarrollo", "diseno"]
+
+def run():
+    print("gola")
+
+if __name__ == '__main__':
+    print("- B I E N V E N I D O S  A  A H O R C A D O S -")
+    run()
---
+++ arrays.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+if __name__ == '__main__':
+   listaUno = []
+   listaUno.append(1)
+
+   listaDos = [2,3,4,5,6]
+
+   listaTres =  listaDos + listaUno
+
+   print(listaUno/5)

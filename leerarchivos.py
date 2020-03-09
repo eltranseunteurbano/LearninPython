7de9c8e2a785a90085c6c0157936fb19bf93843b
Jaime Burbano <burbanojaime98@gmail.com>
2020-03-08 23:57:56 -0500

    CRUD

---
+++ leerarchivos.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+def run():
+    archivo = str(input("Introduzca el archivo que quiere abrir: "))
+    accion = str(input("r = Leer, w = Escribir, a = Añadir, r+ = Leer y escribir: "))
+    try:
+        if(accion == 'r' or accion == 'w' or accion == 'a' or accion == 'r+'):
+            with open (archivo, accion) as prueba:
+                if accion == 'r':
+                    print(prueba.readlines())
+                elif accion == 'w':
+                    print('escribiendo')
+                elif accion == 'a':
+                    print('Añadiendo')
+                elif accion == 'r+':
+                    print('Leyendo y escribiendo')
+        else:
+            print('Ese comando no esta disponible')
+            accion = str(input("r = Leer, w = Escribir, a = Añadir, r+ = Leer y escribir: "))
+                
+    except FileNotFoundError:
+        print('El archivo {} que esta intentando abrir, no existe'. format(archivo))
+if __name__ == '__main__':
+    run()
